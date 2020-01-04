c596129589ccb47e12f585b480024b0346caa8c8
zhoubing <zhoubing19820720@hotmail.com>
2020-01-04 21:47:46 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqlSugar;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        public string no { get; set; }
+        public string name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = "server=localhost;uid=root;pwd=123;database=test",
+                DbType = DbType.MySql,
+                IsAutoCloseConnection = true,
+                InitKeyType = InitKeyType.Attribute
+            });
+            var students = db.Queryable<Student>().ToList();
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine(students[i].no);
+                Console.WriteLine(students[i].name);
+            }
+        }
+    }
+}
